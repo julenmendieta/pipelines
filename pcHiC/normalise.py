1f338c3e12f49b13462c801c87f6c47d60744d6e
julenmendieta <julenmendieta92@gmail.com>
2019-07-23 09:55:41 +0200

    removed redundant controls to avoid division by zero. If it happends means the data is wrong

--- pcHiC/normalise.py
+++ pcHiC/normalise.py
@@ -112,20 +112,23 @@ def frecuenciesNorm(hic_data, resol, regRange, concatemersBin, multResult=100,
 
                 else:
                     # get divider
-                    if concatemersBin[bin1] == 0:
-                        if concatemersBin[bin2] == 0:
-                            divider = 1
-                        else:
-                            divider = concatemersBin[bin2]
-                    elif concatemersBin[bin2] == 0:
-                        divider = concatemersBin[bin1]
-                    else:
-                        divider = concatemersBin[bin1] + concatemersBin[bin2] - hic_data[bin1, bin2]
+                    #if concatemersBin[bin1] == 0:
+                    #    if concatemersBin[bin2] == 0:
+                    #        divider = 1
+                    #    else:
+                    #        divider = concatemersBin[bin2]
+                    #elif concatemersBin[bin2] == 0:
+                    #    divider = concatemersBin[bin1]
+                    #else:
+                    divider = concatemersBin[bin1] + concatemersBin[bin2] - hic_data[bin1, bin2]
 
                     #divider = float(concatemersBin[bin1] + concatemersBin[bin2])
 
-                    if divider == 0:
-                        divider = 1
+                    #if divider == 0:
+                    #    divider = 1
+                    # if divider is 0 it means concatemersBin was taken with another index
+                    #ie just checking a chromosome, or whole genome and here just 
+                    #normalising a file were we loaded one chromosome
                     # if both are zero 
                     norm_data[bin1, bin2] = (hic_data[bin1, bin2] / float(divider)) * multResult
                     norm_data[bin2, bin1] = norm_data[bin1, bin2]
@@ -138,20 +141,20 @@ def frecuenciesNorm(hic_data, resol, regRange, concatemersBin, multResult=100,
                 pass  # Leave it as zero
             else:
                 # get divider
-                if concatemersBin[ke[0]] == 0:
-                    if concatemersBin[ke[1]] == 0:
-                        divider = 1
-                    else:
-                        divider = concatemersBin[ke[1]]
-                elif concatemersBin[ke[1]] == 0:
-                    divider = concatemersBin[ke[0]]
-                else:
-                    divider = concatemersBin[ke[0]] + concatemersBin[ke[1]] - hic_data[ke[0], ke[1]]
+                #if concatemersBin[ke[0]] == 0:
+                #    if concatemersBin[ke[1]] == 0:
+                #        divider = 1
+                #    else:
+                #        divider = concatemersBin[ke[1]]
+                #elif concatemersBin[ke[1]] == 0:
+                #    divider = concatemersBin[ke[0]]
+                #else:
+                divider = concatemersBin[ke[0]] + concatemersBin[ke[1]] - hic_data[ke[0], ke[1]]
 
                 #divider = float(concatemersBin[bin1] + concatemersBin[bin2])
 
-                if divider == 0:
-                    divider = 1
+                #if divider == 0:
+                #    divider = 1
                 # if both are zero 
                 norm_data[ke[0], ke[1]] = hic_data[ke[0], ke[1]] / float(divider)
                 norm_data[ke[1], ke[0]] = norm_data[ke[0], ke[1]]
