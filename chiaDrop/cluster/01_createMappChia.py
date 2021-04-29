c751a74461d69dfadbf40857a54415d151c9d49e
Julen Mendieta Esteban <jmendietaes@hpc-2.unav.es>
2021-04-29 15:09:06 +0200

    changing things around

--- chiaDrop/cluster/01_createMappChia.py
+++ chiaDrop/cluster/01_createMappChia.py
@@ -5,13 +5,13 @@ import time
 filesPath = '/home/jmendietaes/data/2021/chia-drop/fastq/toMapp.txt'
 
 # load genome path
-genome = '/home/jmendietaes/referenceGenomes/mm10_reordered/mm10.reordered.fa'
-#genome = '/home/jmendietaes/referenceGenomes/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa'
+#genome = '/home/jmendietaes/referenceGenomes/mm10_reordered/mm10.reordered.fa'
+genome = '/home/jmendietaes/referenceGenomes/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.fa'
 #genome = '/ssd/genomes/dm6/dm6.fa'
 
 # load GEM index path
-gem_index_path = '/home/jmendietaes/referenceGenomes/mm10_reordered/mm10.reordered.gem'
-#gem_index_path= '/home/jmendietaes/referenceGenomes/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.gem'
+#gem_index_path = '/home/jmendietaes/referenceGenomes/mm10_reordered/mm10.reordered.gem'
+gem_index_path= '/home/jmendietaes/referenceGenomes/GRCh38/GCA_000001405.15_GRCh38_no_alt_analysis_set.gem'
 
 # get output folder
 path = '/home/jmendietaes/data/2021/chia-drop/mapOut/'
@@ -27,10 +27,10 @@ minseq = 25
 maxseq = 130
 
 # get number of CPU to use
-nthreads = 16
+nthreads = 32
 
 # Path for location of scripts
-scriptsPath = ''
+scriptsPath = '/home/jmendietaes/programas/PhD/chiaDrop/cluster/'
 
 #####################################################
 
@@ -58,16 +58,6 @@ fecha = time.strftime("%d-%m-%Y")
 runfile = '%s%s.Mappsarray'%(path, fecha)
 ## Create the file with the commands to be run in the array
 fout=open(runfile,'w')
-parser.add_argument('-fq1','--fastq1', help = 'Path to fasq file of read 1',required=True)
-parser.add_argument('-fq2','--fastq2', help = 'Path to fasq file of read 2', required=True)
-parser.add_argument('-gnm','--genome', help = 'Path to reference genome', required=True)
-parser.add_argument('-gi','--gemindex', help = 'Path to GEM index file', required=True)
-parser.add_argument('-op','--outpath',help='Path to output folder', required=True)
-parser.add_argument('-opt','--tempdir',help='Path for temporal output folder', required=False)
-parser.add_argument('-mins','--minseq',help='Minimum bp length to map (def. 25bp)', required=False)
-parser.add_argument('-maxs','--maxseq',help='Maximum bp length to map (Min read length)', required=True)
-parser.add_argument('-t','--threads',help='Number of CPU to use', required=False)
-
 for nr in range(0, njobs):
 	cmd=''
 	cmd+='%s01_mapper.py -fq1 %s '%(scriptsPath, reads[nr][0])
@@ -76,9 +66,9 @@ for nr in range(0, njobs):
 	cmd+= '-gi %s '%gem_index_path
 	cmd+= '-op %s '%path
 	cmd+= '-opt %s '%tempOut
-    cmd+= '-mins %s '%minseq
-    cmd+= '-maxs %s '%maxseq
-    cmd+= '-t %s'%nthreads
+    	cmd+= '-mins %s '%minseq
+    	cmd+= '-maxs %s '%maxseq
+    	cmd+= '-t %s'%nthreads
 	#cmd+= '-t %s '%jobTime  # will add it in the arrayMod.jobs so we can update it later
 	cmd+= '\n'
 	fout.write(cmd)
